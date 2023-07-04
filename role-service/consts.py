e760abe469cd10760c34ced54b2dc4b9b789e8c0
wujohns <wujohns@163.com>
2023-07-05 07:00:53 +0800

    luotuo-bert embedding 的引入与处理

--- role-service/consts.py
+++ role-service/consts.py
@@ -2,6 +2,7 @@ import os
 from env import openai_key, openai_proxy
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
+from custom_embedding import LuotuoBertEmbeddings
 
 curPath = os.path.dirname(os.path.abspath(__file__))
 def getAbsPath (relativePath):
@@ -16,7 +17,7 @@ def getAbsPath (relativePath):
 # 2. paraphrase-multilingual-MiniLM-L12-v2: huggingface 推出的多语言相似度查询模型
 # 3. luotuo-bert: 李鲁鲁团队训练的中文相似度查询模型(这个需要单独修改代码结构，需要进行额外的代码解析编写工作)
 # embeddings = HuggingFaceEmbeddings(model_name=getAbsPath('../hg-repos/embedding-models/paraphrase-multilingual-MiniLM-L12-v2'))
-embeddings = HuggingFaceEmbeddings(model_name=getAbsPath('../hg-repos/embedding-models/all-MiniLM-L6-v2'))
+# embeddings = HuggingFaceEmbeddings(model_name=getAbsPath('../hg-repos/embedding-models/all-MiniLM-L6-v2'))
 
 # openai 暂时被限制使用，这里需要考虑使用其他方法进行处理
 # embeddings = OpenAIEmbeddings(
@@ -24,7 +25,7 @@ embeddings = HuggingFaceEmbeddings(model_name=getAbsPath('../hg-repos/embedding-
 #   openai_proxy=openai_proxy
 # )
 
-# 参考 https://docs.trychroma.com/embeddings 尝试编写自定义 embedding
+embeddings = LuotuoBertEmbeddings()
 
 # 服务器配置
 host = '0.0.0.0'
---
+++ role-service/custom_embedding.py
@@ -0,0 +1,53 @@
+# 自定义的 embedding 封装
+import os
+import torch
+from typing import List, Any
+from pydantic import BaseModel, Extra
+from langchain.embeddings.base import Embeddings
+from argparse import Namespace
+from transformers import AutoModel, AutoTokenizer
+
+curPath = os.path.dirname(os.path.abspath(__file__))
+def getAbsPath (relativePath):
+  joinPath = os.path.join(curPath, relativePath)
+  return os.path.normpath(
+    os.path.abspath(joinPath)
+  )
+
+# luotuo_bert 模型相关
+luotuo_bert_model_path = getAbsPath('../hg-repos/embedding-models/luotuo-bert')
+luotuo_bert_tokenizer = AutoTokenizer.from_pretrained(luotuo_bert_model_path)
+luotuo_bert_model_args = Namespace(
+  do_mlm=None, pooler_type="cls", temp=0.05,
+  mlp_only_train=False, init_embeddings_model=None
+)
+luotuo_bert_model = AutoModel.from_pretrained(
+  luotuo_bert_model_path,
+  trust_remote_code=True,
+  model_args=luotuo_bert_model_args
+)
+luotuo_bert_model.eval()
+
+# TODO 将模型载入到 gpu 加快计算速度
+class LuotuoBertEmbeddings(BaseModel, Embeddings):
+  def __init__(self, **kwargs: Any):
+    super().__init__(**kwargs)
+
+  class Config:
+    """Configuration for this pydantic object."""
+    extra = Extra.forbid
+
+  def embed_documents(self, texts: List[str]) -> List[List[float]]:
+    texts = [text[:512] for text in texts]
+    inputs = luotuo_bert_tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
+    with torch.no_grad():
+      embeddings = luotuo_bert_model(**inputs, output_hidden_states=True, return_dict=True, sent_emb=True).pooler_output
+    return embeddings.tolist()
+
+  def embed_query(self, text: str) -> List[float]:
+    text = text[:512]
+    texts = [text]
+    inputs = luotuo_bert_tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
+    with torch.no_grad():
+      embeddings = luotuo_bert_model(**inputs, output_hidden_states=True, return_dict=True, sent_emb=True).pooler_output
+    return embeddings.tolist()[0]
